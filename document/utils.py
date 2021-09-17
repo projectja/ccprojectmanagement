f23669c186ec0822f931d894f74dc941d62a6923
Dave <davidalejandrodand@gmail.com>
2021-09-17 10:30:32 -0400

    Se simplifico el context que se pasa

--- document/utils.py
+++ document/utils.py
@@ -1,31 +1,21 @@
-from django.template.loader import get_template
+from django.template.loader import render_to_string
 
 class AcctionDocument:
 
     def generarHTML(projects_selected):
-        list_document_html = list()
+        dict_doc_html = dict()
         # print(data.documento_solicitante, data.documento_representante)
         
         # Se recorre el queryset de los registros en `projects`
         # se crea un contexto por cada registro, y se genera 
-        # un template que será almacenado en list_document_html
+        # un template que será almacenado en dict_doc_html
         for p in projects_selected:
-            template = get_template('Plantilla Base/document.html')
-
             context = {
-                'programa': p.programa,
-                'solicitante': p.solicitante,
-                'documento_solicitante': p.documento_solicitante,
-                'representante': p.representante,
-                'documento_representante': p.documento_representante,
-                'correo_representante': p.correo_representante,
+                'project': p,
             }
+            # Se utiliza render_to_string para tomar el codigo html
+            # y convertirlo en un objeto SafeString
+            document_html = render_to_string('Plantilla Base/document.html', context)
+            dict_doc_html[p.solicitante] = document_html
 
-            # context = {
-            #     'project': p,
-            # }
-    
-            document_html = template.render(context)
-            list_document_html.append(document_html)
-        
-        return list_document_html
+        return dict_doc_html
